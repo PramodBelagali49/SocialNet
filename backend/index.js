e615a80abd3cee6b06416d7497cfcb755724ccb9
PramodBelagali49 <cse.pramod.b2@gmail.com>
2025-05-17 22:37:59 +0530

    including the libraries and adding middlewares and basic setup

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,35 @@
-import express from "express"
+import dotenv from "dotenv"
+if(process.env.NODE_ENV != "production"){
+    dotenv.config({ path: '../.env' })
+}
+
+import express, { urlencoded } from "express"
 import cors from "cors"
 import cookieParser from "cookie-parser"
-const app=express()
+import connectDB from "./utils/connectDB.js"
 
-app.listen(5600,()=>{
-    console.log("server listening to port 5600");
-})
+const app=express()
+// middlewares
+app.use(express.json());
+app.use(cookieParser());
+app.use(urlencoded({extended:true}));
+
+const corsOptions={
+    origin:"http://localhost:5173",
+    credentials:true
+}
+app.use(cors(corsOptions));
+
 
 app.get("/",(req,resp)=>{
-    resp.send("Hey bye bye ig!!");
+    return resp.status(200).json({
+        message:"I'm from backend",
+        success:true
+    })
+})
+
+const port=8000;
+app.listen(port,()=>{
+    connectDB();
+    console.log(`server listening to port ${port}`);
 })
