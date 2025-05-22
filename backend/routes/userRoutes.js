06a03f0ac7ea67dd54bffc3043f492b50c87352e
PramodBelagali49 <cse.pramod.b2@gmail.com>
2025-05-23 02:21:42 +0530

    added all the userRoutes & tested by using Postaman

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,16 @@
+import express from "express";
+import { editProfile, followUnfollow, getProfile, getSuggestedUsers, login, logout, register } from "../controllers/userController.js";
+import isAuthenticated from "../middlewares/middleware.js";
+import upload from "../middlewares/multer.js";
+
+const router=express.Router();
+
+router.route("/register").post(register);
+router.route("/login").post(login);
+router.route("/logout").get(logout);
+router.route("/:id/profile").get(isAuthenticated,getProfile);
+router.route("/profile/edit").post(isAuthenticated,upload.single("profilePicture"),editProfile);
+router.route("/suggested").get(isAuthenticated,getSuggestedUsers);
+router.route("/followUnfollow/:id").post(isAuthenticated,followUnfollow);
+
+export default router;
