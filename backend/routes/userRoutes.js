c526bd2a79acc62db1df52696d6ca8b1a9277ba0
PramodBelagali49 <cse.pramod.b2@gmail.com>
2025-05-26 21:02:43 +0530

    userRoutes fix and renamed middleware.js to isAuthenticated

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { editProfile, followUnfollow, getProfile, getSuggestedUsers, login, logout, register } from "../controllers/userController.js";
-import isAuthenticated from "../middlewares/middleware.js";
+import isAuthenticated from "../middlewares/isAuthenticated.js";
 import upload from "../middlewares/multer.js";
 
 const router=express.Router();
