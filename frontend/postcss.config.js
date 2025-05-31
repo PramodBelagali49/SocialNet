61e1f555ea28635c8f1e16bf011398fd043d6b18
PramodBelagali49 <cse.pramod.b2@gmail.com>
2025-05-31 10:45:17 +0530

    setting up the frontend for tailwind,react and shadcn

---
+++ frontend/postcss.config.js
@@ -0,0 +1,7 @@
+// Correct config for Tailwind CSS v4+
+export const config_tailwind = {
+  plugins: {
+    '@tailwindcss/postcss': {},
+    autoprefixer: {},
+  },
+}
---
+++ frontend/src/components/Signup.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Button } from "./ui/Button";
+
+const Signup=()=>{
+    return(
+        <>
+            <Button>SignUp</Button>
+        </>
+    )
+}
+export default Signup;
