2bd1b07982198305e38f2a15485cebc436ac3d42
PramodBelagali49 <cse.pramod.b2@gmail.com>
2025-06-10 00:44:10 +0530

    getting started with state management using RTK

---
+++ frontend/src/redux/authSlice.js
@@ -0,0 +1,14 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const authSlice=createSlice({
+    name:"auth",
+    initialState:null,
+    reducers:{
+        setAuthUser:(state,action)=>{
+            state.user=action.payload
+        }
+    }
+});
+
+export const {setAuthUser}=authSlice.actions
+export default authSlice.reducer
