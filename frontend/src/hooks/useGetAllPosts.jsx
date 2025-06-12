6f6b504eca9172fd99601547d984fc5797d7845c
PramodBelagali49 <cse.pramod.b2@gmail.com>
2025-06-12 10:38:53 +0530

    custom hooks,a hook to fetch all the posts

---
+++ frontend/src/hooks/useGetAllPosts.jsx
@@ -0,0 +1,23 @@
+import { setPosts } from "@/redux/postSlice";
+import axios from "axios";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+
+export const useGetAllPosts=()=>{
+    const dispatch=useDispatch();
+    useEffect(()=>{
+        const fetchAllPosts=async()=>{
+            try {
+                const res=await axios.get("http://localhost:3600/api/posts",{withCredentials:true});
+                if(res.data.success){
+                    console.log(res.data.posts);
+                    dispatch(setPosts(res.data.posts));
+                }
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        fetchAllPosts();
+    },[])
+}
+export default useGetAllPosts;
