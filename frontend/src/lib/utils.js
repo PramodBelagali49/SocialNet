2174997df7c0f56b416249f019a4f153291c3485
PramodBelagali49 <cse.pramod.b2@gmail.com>
2025-06-12 10:41:13 +0530

    function for converting file to data url

--- frontend/src/lib/utils.js
+++ frontend/src/lib/utils.js
@@ -1,6 +1,26 @@
 import { clsx } from "clsx";
-import { twMerge } from "tailwind-merge"
+import { useState } from "react";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs) {
   return twMerge(clsx(inputs));
+}
+
+export const fileAsDataURL = async (file) => {
+  return new Promise((resolve, reject) => {
+    if (!file){
+        reject(new Error("File does NOT exist"));
+        return;
+    }
+    const reader = new FileReader();
+
+    reader.onload = () => {
+      resolve(reader.result);
+    };
+    reader.onerror = (err) => {
+      reject(err);
+    };
+
+    reader.readAsDataURL(file);
+  });
 }
