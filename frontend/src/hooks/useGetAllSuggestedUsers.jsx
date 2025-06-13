97984a332f46bbfaeb82f8e38f835cdd8d8c0c8f
PramodBelagali49 <cse.pramod.b2@gmail.com>
2025-06-14 00:31:56 +0530

    displaying user suggestions

---
+++ frontend/src/hooks/useGetAllSuggestedUsers.jsx
@@ -0,0 +1,23 @@
+import { setSuggestedUsers } from "@/redux/authSlice";
+import axios from "axios";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+
+export const useGetAllSuggestedUsers=()=>{
+    const dispatch=useDispatch();
+    useEffect(()=>{
+        const fetchSuggestions=async()=>{
+            try {
+                const res=await axios.get("http://localhost:3600/api/user/suggested",{withCredentials:true});
+                if(res.data.success){
+                    // console.log(res.data.users);
+                    dispatch(setSuggestedUsers(res.data.users));
+                }
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        fetchSuggestions();
+    },[])
+}
+export default useGetAllSuggestedUsers;
