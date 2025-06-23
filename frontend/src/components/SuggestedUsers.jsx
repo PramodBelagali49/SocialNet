412830b9d53872fdfb2244095dab346140d6e5d3
PramodBelagali49 <cse.pramod.b2@gmail.com>
2025-06-23 22:57:05 +0530

    get the suggestedUsers in the rightSideBar

--- frontend/src/components/SuggestedUsers.jsx
+++ frontend/src/components/SuggestedUsers.jsx
@@ -27,7 +27,7 @@ const SuggestedUsers = () => {
                                     <Link to={`/profile/${user?._id}`}>
                                         <Avatar>
                                             <AvatarImage src={user?.profilePicture} alt="post_image" />
-                                            <AvatarFallback>CN</AvatarFallback>
+                                            <AvatarFallback className="w-full h-full">CN</AvatarFallback>
                                         </Avatar>
                                     </Link>
                                     <div>
