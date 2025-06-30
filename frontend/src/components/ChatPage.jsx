6961a047d530cb3480cfce0f68aee5b464b0589f
PramodBelagali49 <cse.pramod.b2@gmail.com>
2025-06-30 11:15:17 +0530

    chatPage and messagesPage for chatting

--- frontend/src/components/ChatPage.jsx
+++ frontend/src/components/ChatPage.jsx
@@ -1,76 +1,107 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { setSelectedUserForChat } from '@/redux/authSlice';
 import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar';
 import { Button } from './ui/Button';
 import { MessageCircleCode } from 'lucide-react';
 import Messages from './Messages';
+import axios from 'axios';
+import { setMessages } from '@/redux/chatSlice';
 
 
 function ChatPage() {
     const {user, suggestedUsers, selectedUserForChat}=useSelector(store=>store.auth);
-    const isOnline=false;
     const dispatch=useDispatch();
-  return (
-    <div className='flex h-screen'>
-        <section className='w-full md:w-1/4 ml-60'>
-            <h1 className='font-bold mb-4 px-3 text-xl'>{user?.username}</h1>
-            <h4 className='font-semibold ml-2'>Select a user to chat</h4>
-            <hr className='mb-4 border-gray-300 w-70'/>
-            <div className='overflow-y-auto h-[80vh] w-70'>
-                {
-                    suggestedUsers.map((suggestedUser) => {
-                        // const isOnline = onlineUsers.includes(suggestedUser?._id);
-                        return (
-                            <div key={suggestedUser?._id} onClick={() => dispatch(setSelectedUserForChat(suggestedUser))} className='flex gap-3 items-center p-3 hover:bg-gray-50 cursor-pointer'>
-                                <Avatar className='w-12 h-12'>
-                                    <AvatarImage src={suggestedUser?.profilePicture} />
-                                    <AvatarFallback>CN</AvatarFallback>
-                                </Avatar>
-                                <div className='flex flex-col'>
-                                    <span className='font-medium'>{suggestedUser?.username}</span>
-                                    <span className={`text-xs font-bold ${isOnline ? 'text-green-600' : 'text-red-600'} `}>{isOnline ? 'online' : 'offline'}</span>
-                                </div>
-                            </div>
-                        )
-                    })
-                }
-            </div>
-        </section>
-        {
-            selectedUserForChat ? (
-                <section className='flex-1 border-1 border-1-gray-300 flex flex-col h-full mt-2 mr-10 mb-2'>
-                    <div className='flex gap-3 items-center px-3 py-2 border-b border-gray-300 sticky top-0 bg-white z-10'>
-                        <Avatar>
-                            <AvatarImage src={selectedUserForChat?.profilePicture}  alt="profilephoto" />
-                            <AvatarFallback>CN</AvatarFallback>
-                        </Avatar>
-                        <div className='flex flex-col'>
-                            <span>{selectedUserForChat?.username}</span>
-                        </div>
-                    </div>
-                    {/* Messages */}
-                    <Messages selectedUserForChat={selectedUserForChat}/>
-                    <div className='flex items-center p-4 border-t border-t-gray-300'>
-                        <input
-                            type="text"
-                            className="flex-1 mr-2 h-9 border border-gray-300 focus:outline-none focus:ring-0 focus:border-gray-300 placeholder:text-sm px-2 rounded"
-                            placeholder="message something"
-                        />
+    const {onlineUsers,messages}=useSelector(store=>store.chat)
 
-                        <Button variant="outline">Send</Button>
-                    </div>
-                    <h3><div onClick={()=>dispatch(setSelectedUserForChat(null))} className='cursor-pointer'>Unselect the user</div></h3>
-                </section>
-            ) : (
-                <div className='flex flex-col items-center justify-center mx-auto'>
-                    <MessageCircleCode className='w-32 h-32 my-4'/>
-                    <h1 className='font-medium text-xl'>Your messages</h1>
-                    <span>Send a message to start the conversation</span>
-                </div>
-            )
+    const [textMessage,setTextMessage]=useState("");
+
+    const messageHandler=async()=>{
+        const res=await axios.post(`http://localhost:3600/api/messages/send/${selectedUserForChat?._id}`,{message:textMessage},{
+            headers:{
+                "Content-Type":"application/json"
+            },
+            withCredentials:true
+        });
+        if(res.data.success){
+            dispatch(setMessages([...messages,res.data.newMessage]));
+            setTextMessage("");
         }
-    </div>
-  )
+    }
+
+    useEffect(()=>{
+        return()=>{
+            dispatch(setSelectedUserForChat(null));
+        }
+    },[])
+    return (
+        <div className='flex h-screen'>
+            <section className='w-full md:w-1/4 ml-60'>
+                <h1 className='font-bold mb-4 px-3 text-xl'>{user?.username}</h1>
+                <h4 className='font-semibold ml-2'>Select a user to chat</h4>
+                <hr className='mb-4 border-gray-300 w-70'/>
+                <div className='overflow-y-auto h-[80vh] w-70'>
+                    {
+                        suggestedUsers.map((suggestedUser) => {
+                            const isOnline = onlineUsers.includes(suggestedUser?._id);
+                            return (
+                                <div key={suggestedUser?._id} onClick={() => dispatch(setSelectedUserForChat(suggestedUser))} className='flex gap-3 items-center p-3 hover:bg-gray-50 cursor-pointer'>
+                                    <Avatar className='w-12 h-12'>
+                                        <AvatarImage src={suggestedUser?.profilePicture} />
+                                        <AvatarFallback>CN</AvatarFallback>
+                                    </Avatar>
+                                    <div className='flex flex-col'>
+                                        <span className='font-medium'>{suggestedUser?.username}</span>
+                                        <span className={`text-xs font-bold ${isOnline ? 'text-green-600' : 'text-red-600'} `}>{isOnline ? 'online' : 'offline'}</span>
+                                    </div>
+                                </div>
+                            )
+                        })
+                    }
+                </div>
+            </section>
+            {
+                selectedUserForChat ? (
+                    <section className='flex-1 border-1 border-1-gray-300 flex flex-col h-full mt-2 mr-10 mb-2'>
+                        <div className='flex gap-3 items-center px-3 py-2 border-b border-gray-300 sticky top-0 bg-white z-10'>
+                            <Avatar>
+                                <AvatarImage src={selectedUserForChat?.profilePicture}  alt="profilephoto" />
+                                <AvatarFallback>CN</AvatarFallback>
+                            </Avatar>
+                            <div className='flex flex-col'>
+                                <span>{selectedUserForChat?.username}</span>
+                            </div>
+                        </div>
+                        {/* Messages */}
+                        <Messages selectedUserForChat={selectedUserForChat}/>
+                        <div className='flex items-center p-4 border-t border-t-gray-300'>
+                            <input
+                                type="text"
+                                className="flex-1 mr-2 h-9 border border-gray-300 focus:outline-none focus:ring-0 focus:border-gray-300 placeholder:text-sm px-2 rounded"
+                                placeholder="message something"
+                                value={textMessage}
+                                onChange={(e)=>setTextMessage(e.target.value)}
+                                 onKeyDown={(e) => {
+                                    if (e.key === "Enter" && textMessage.trim()) {
+                                        e.preventDefault();
+                                        messageHandler();  // send message
+                                    }
+                                }}
+                            />
+
+                            <Button variant="outline" onClick={messageHandler} className="cursor-pointer">Send</Button>
+                        </div>
+                        {/* <h3><div onClick={()=>dispatch(setSelectedUserForChat(null))} className='cursor-pointer'>Unselect the user</div></h3> */}
+                    </section>
+                ) : (
+                    <div className='flex flex-col items-center justify-center mx-auto'>
+                        <MessageCircleCode className='w-32 h-32 my-4'/>
+                        <h1 className='font-medium text-xl'>Your messages</h1>
+                        <span>Send a message to start the conversation</span>
+                    </div>
+                )
+            }
+        </div>
+    )
 }
 export default ChatPage
--- frontend/src/components/Messages.jsx
+++ frontend/src/components/Messages.jsx
@@ -1,26 +1,46 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar'
 import { Link } from 'react-router-dom'
 import { Button } from './ui/Button'
+import { useSelector } from 'react-redux'
+import useGetAllMessages from '@/hooks/useGetAllMessages'
+import useGetRTM from '@/hooks/useGetRTM'
 
 function Messages({selectedUserForChat}) {
-  return (
-    <div className='overflow-y-auto flex-1 p-4'>
-        <div className='flex justify-center'>
-            <div className='flex flex-col items-center justify-center'>
-                <Avatar className="h-20 w-20">
-                    <AvatarImage
-                    src={selectedUserForChat?.profilePicture}
-                    alt="profilephoto"
-                    />
-                    <AvatarFallback className="w-[150px] h-[150px] flex items-center justify-center bg-gray-200"></AvatarFallback>
-                </Avatar>
-                <span>{selectedUserForChat?.username}</span>
-                <Link to={`/profile/${selectedUserForChat?._id}`}><Button className="h-8 my-2" variant="secondary">View profile</Button></Link>
-            </div>
-        </div>
-    </div>
-  )
+    useGetRTM();
+    useGetAllMessages();
+    const {messages}=useSelector(store=>store.chat);
+    const {user}=useSelector(store=>store.auth);
+    return (
+      <div className='overflow-y-auto flex-1 p-4'>
+          <div className='flex justify-center flex-col'>
+              <div className='flex flex-col items-center justify-center'>
+                  <Avatar className="h-20 w-20">
+                      <AvatarImage
+                      src={selectedUserForChat?.profilePicture}
+                      alt="profilephoto"
+                      />
+                      <AvatarFallback className="w-[150px] h-[150px] flex items-center justify-center bg-gray-200"></AvatarFallback>
+                  </Avatar>
+                  <span>{selectedUserForChat?.username}</span>
+                  <Link to={`/profile/${selectedUserForChat?._id}`}><Button className="h-8 my-2 cursor-pointer bg-gray-300" variant="secondary">View profile</Button></Link>
+              </div>
+              <div>
+                {
+                  messages && messages.map((message)=>{
+                    return(
+                          <div className={`flex ${user?._id==message.senderId ? 'justify-end' : 'justify-start'}`} key={message?._id}>
+                              <div className={`p-2 rounded-lg max-w-xs break-words ${message.senderId === user?._id ? 'bg-blue-500 text-white my-1' : 'bg-gray-200 text-black my-2'}`}>
+                                  {message.message}
+                              </div>
+                          </div>
+                    )
+                  })
+                }
+              </div>
+          </div>
+      </div>
+    )
 }
 
 export default Messages
