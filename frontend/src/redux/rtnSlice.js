656688217a8222f8a490de02bb06119a6c6da5f7
PramodBelagali49 <cse.pramod.b2@gmail.com>
2025-07-03 16:46:58 +0530

    real time notifications

---
+++ frontend/src/redux/rtnSlice.js
@@ -0,0 +1,20 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const rtnSlice=createSlice({
+    name:"realTimeNotification",
+    initialState:{
+        likeNotifications:[]
+    },
+    reducers:{
+        // actions
+        setLikeNotifications:(state,action)=>{
+            if(action.payload.type=="like"){
+                state.likeNotifications.push(action.payload.userDetails);
+            }else if(action.payload.type=="dislike"){
+                state.likeNotifications=state.likeNotifications.filter((item)=>item.userId !== action.payload.userId)
+            }
+        }
+    }
+})
+export const {setLikeNotifications}=rtnSlice.actions
+export default rtnSlice.reducer
